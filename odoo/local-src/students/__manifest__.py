a125ef09f50767a7e0cc7a0de508e0e6be1ae78c
matski <nathan.grosjean@etu.univ-smb.fr>
2023-03-10 16:48:51 +0100

    Initial commit

---
+++ odoo/local-src/students/__manifest__.py
@@ -0,0 +1,15 @@
+{
+ "name": "Gestion des étudiants",
+ "version": "0.8",
+ "category": "Generic Modules/Others",
+ "description": """ Test création module gestion des étudiants Odoo v14""",
+ "author": "Moi",
+ "data": ["data/students_training_data.xml",
+"data/students_student_data.xml",
+"views/students_views.xml",
+"views/training_views.xml",
+"views/mark_views.xml",],
+ "depends": ["base"],
+ "installable": True,
+ "auto_install": False,
+}
